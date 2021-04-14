a99212f5bea83d3881f8375cecf73f3b22c66058
RachitVargas1 <>
2021-04-14 00:24:50 +0000

    Cambios

--- main.clj
+++ main.clj
@@ -1,20 +1,22 @@
 (def valor_ascii (int \a))
 
-(defn positivo[a b]
-(if (> a b) (- a b) (- b a)))
-
 (defn valor_entero [valor]
   (- (int valor) valor_ascii))
 
+(defn positivo[a b]
+(cond 
+(> a b)(- a b)
+:else (- b a)))
+
 (defn valor_caracter [num]
   (def numero_p (positivo num 0))
-  (if (<= numero_p 25)(char (+ numero_p valor_ascii))))
+  (cond (<= numero_p 25) (char (+ numero_p valor_ascii))
+  :else "Ha ocurrido un error"))
 
 (defn numero_corridas[char key]
   (valor_caracter (mod (+ (valor_entero char) key) 26)))
 
-(def encriptar (mapv #(numero_corridas % 3) "mario"))
-
-(def resultado (apply str encriptar))
+(def cifrar (mapv #(numero_corridas % 2)"Rachit Vargas"))
+(def resultado (apply str cifrar))
 
 println(str "Cifrado cesar: " resultado)
