a851530e73c425738195d373a7f3d8812c432f54
RachitVargas <65442367+RachitVargas@users.noreply.github.com>
2021-04-14 22:28:20 -0600

    Update main.clj

--- main.clj
+++ main.clj
@@ -1,27 +1,19 @@
 (def valor_ascii (int \a))
 
-(defn valor_entero [valor]
-  (-(int valor)valor_ascii))
-
-(defn positivo[a b]
-  (cond 
-  (> a b)(- a b)
-  :else (- b a)))
-
 (defn valor_caracter [num]
-  (def numero_positivo (positivo num 0))
+  (def numero_positivo 
+  (cond (> num 0)(- num 0) :else (- 0 num)))  
   (cond (<= numero_positivo 25) (char (+ numero_positivo valor_ascii))
   :else "Ha ocurrido un error"))
 
 (defn numero_corridas[char key]
-  (valor_caracter (mod (+ (valor_entero char) key) 26)))
+  (valor_caracter (mod (+ (-(int char)valor_ascii) key) 26)))
 
-(def mensaje "Mario")
-(def cantidad_corridas 2)
+(def mensaje "Rachit")
+(def cantidad_corridas 3)
 
 (def codificar (mapv #(numero_corridas % cantidad_corridas)mensaje))
 (def cifrado (apply str codificar))
 
 println(str "Mensaje original: " mensaje)
 println(str "Mensaje encriptado con el Cifrado Cesar: " cifrado)
-
