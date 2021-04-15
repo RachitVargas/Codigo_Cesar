21e4433945c8d15a34705bbcc132417fcc3212f2
RachitVargas1 <>
2021-04-15 05:26:46 +0000

    Cambios

--- main.clj
+++ main.clj
@@ -1,12 +1,11 @@
 (defn valor_caracter [numero]
   (def numero_positivo 
   (cond (> numero 0)(- numero 0) :else (- 0 numero)))  
-  (cond (<= numero_positivo 25) (char (+ numero_positivo valor_ascii))
+  (cond (<= numero_positivo 25) (char (+ numero_positivo (int \a)))
   :else "Ha ocurrido un error"))
 
 (defn rotor[char key]
-  (def valor_ascii (int \a))
-  (valor_caracter (mod (+ (-(int char)valor_ascii) key) 26)))
+  (valor_caracter(mod (+ (-(int char)(int \a)) key) 26)))
 
 (def mensaje "Rachit")
 (def cantidad_corridas 2)
